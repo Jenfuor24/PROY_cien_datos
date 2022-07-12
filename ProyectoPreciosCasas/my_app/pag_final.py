541cf3d43b07fc5285c2c5848148dadf4d89d165
Jenfuor24 <105056493+Jenfuor24@users.noreply.github.com>
2022-07-11 22:26:32 -0500

    Update pag_final.py

--- ProyectoPreciosCasas/my_app/pag_final.py
+++ ProyectoPreciosCasas/my_app/pag_final.py
@@ -35,43 +35,6 @@ def get_data():
 data = get_data()
 data_ref = data.copy()
 
-year_col, continent_col, log_x_col = st.columns([5, 5, 5])
-with year_col:
-    year_choice = st.slider(
-        "What year would you like to examine?",
-        min_value=1952,
-        max_value=2007,
-        step=5,
-        value=2007,
-    )
-with continent_col:
-    continent_choice = st.selectbox(
-        "What continent would you like to look at?",
-        ("All", "Asia", "Europe", "Africa", "Americas", "Oceania"),
-    )
-with log_x_col:
-    log_x_choice = st.checkbox("Log X Axis?")
-# -- Read in the data
-df = px.data.gapminder()
-# -- Apply the year filter given by the user
-filtered_df = df[(df.year == year_choice)]
-# -- Apply the continent filter
-if continent_choice != "All":
-    filtered_df = filtered_df[filtered_df.continent == continent_choice]
-# -- Create the figure in Plotly
-fig = px.scatter(
-    filtered_df,
-    x="gdpPercap",
-    y="lifeExp",
-    size="pop",
-    color="continent",
-    hover_name="country",
-    log_x=log_x_choice,
-    size_max=60,
-)
-fig.update_layout(title="GDP per Capita vs. Life Expectancy")
-# -- Input the Plotly chart to the Streamlit interface
-st.plotly_chart(fig, use_container_width=True)
 
 st.sidebar.markdown("# Parámetros")
 data['date'] = pd.to_datetime(data['date'], format = '%Y-%m-%d').dt.date
@@ -349,5 +312,43 @@ with col2:
           fig.legend(title='Tipo de propiedad', loc='upper right', labels=['Apartamento', 'Casa','Estudio'])
           fig = fig.figure
           st.pyplot(fig)
+          
+ year_col, continent_col, log_x_col = st.columns([5, 5, 5])
+with year_col:
+    year_choice = st.slider(
+        "Año de construccion?",
+        min_value=1900,
+        max_value=2007,
+        step=5,
+        value=2007,
+    )
+with continent_col:
+    continent_choice = st.selectbox(
+        "What continent would you like to look at?",
+        ("Price", "bedrooms", "bathrooms", "floors", "waterfront", "sqft_living"),
+    )
+
+# -- Read in the data
+data = pd.read_csv('kc_house_data.csv')
+# -- Apply the year filter given by the user
+filtered_df = df[(df.year == year_choice)]
+# -- Apply the continent filter
+if continent_choice != "All":
+    filtered_df = filtered_df[filtered_df.continent == continent_choice]
+# -- Create the figure in Plotly
+fig = px.scatter(
+    filtered_df,
+    x="gdpPercap",
+    y="lifeExp",
+    size="pop",
+    color="continent",
+    hover_name="country",
+    log_x=log_x_choice,
+    size_max=60,
+)
+fig.update_layout(title="Price vs. floors")
+# -- Input the Plotly chart to the Streamlit interface
+st.plotly_chart(fig, use_container_width=True)
+
 
 
