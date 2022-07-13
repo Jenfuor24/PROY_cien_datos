8e0124c001bfdbf48f35b4eb28eb32e99e0d0359
Jenfuor24 <105056493+Jenfuor24@users.noreply.github.com>
2022-07-12 19:36:04 -0500

    Update pag_final.py

--- ProyectoPreciosCasas/my_app/pag_final.py
+++ ProyectoPreciosCasas/my_app/pag_final.py
@@ -320,11 +320,7 @@ col1, col2, col3 = st.columns([5, 5, 20])
 # with col2:
 # st.image("streamlit.png", width=200)
 # -- Put the title in the last column
-with col3:
-    st.title("Streamlit Demo")
-    st.write("[Tutorial](https://jovid.win/crear-un-dashboard-con-streamlit/) - [Github](https://github.com/jdzuri/streamlit-dashboard)")
-# -- We use the first column here as a dummy to add a space to the left
-# -- Get the user input
+
 year_col, continent_col, log_x_col = st.columns([5, 5, 5])
 with year_col:
     year_choice = st.slider(
